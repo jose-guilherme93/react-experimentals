a4ec69c4efac2151d426e9f0162d281b5e454807
jose-guilherme93 <jose-guilherme93@hotmail.com>
2023-06-30 19:05:34 -0300

    refactor: adding data file for fakeData

---
+++ src/data/challengesData.ts
@@ -0,0 +1,36 @@
+const pathTree = "challenges"
+
+export const challenges = [
+   
+    {   
+        title: 'profile-settings',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo provident eveniet tenetur?',
+        buttonName: 'profile-settings',
+        href: `${pathTree}/profile-settings`
+   },
+   {    title: 'transport-widget',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo provident eveniet tenetur?',
+        buttonName: 'transport-widget',
+        href: `${pathTree}/transport-widget`
+    },
+    {   title: 'shopping-cart',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo provident eveniet tenetur?',
+        buttonName: 'shopping-cart',
+        href: `${pathTree}/shopping-cart`
+    },
+    {   title: 'shopping-cart',
+        description: 'Lorem ipsum dolor sit amet, conseerg freoufgerf lewskfnnw fiwe newwei fweoiuf weoúif weoiufewhoihewohfewo  o erfwerfg wegrg rg rgq rg rgrewgrgr  r grqewgrgrqg ih woefiu weooictetur adipisicing elit. Explicabo provident eveniet tenetur?',
+        buttonName: 'shopping-cart',
+        href: `${pathTree}/shopping-cart`
+    },
+    {   title: 'input-search',
+        description: 'An input for search person name, city and age, with a filter radio. ',
+        buttonName: 'input-search',
+        href: `${pathTree}/input-search`
+    },
+    {   title: 'todo-list',
+        description: 'An Todo App for taks management ',
+        buttonName: 'todo-list',
+        href: `${pathTree}/todo-list`
+    },
+  ]
---
+++ src/data/inputSearchData.ts
@@ -0,0 +1,69 @@
+export interface  PersonProps   {
+    nome: string;
+    cidade: string;
+    idade:  number;
+}
+
+export const fakeApi: PersonProps[] = [ 
+  {
+    nome: "roberto",
+    idade: 23,
+    cidade: "belo horizonte"
+  },
+  {
+    nome: "clara maria",
+    idade: 45,
+    cidade: "turiaçu"
+  },
+  {
+    nome: "Pessoa 3",
+    idade: 67,
+    cidade: "curitiba"
+  },
+  {
+    nome: "joão",
+    idade: 18,
+    cidade: "bequimão"
+  },
+  {
+    nome: "Pedro",
+    idade: 32,
+    cidade: "Cidade operária"
+  },
+  {
+    nome: "fernanda",
+    idade: 54,
+    cidade: "cababeira"
+  },
+  {
+    nome: "henrique",
+    idade: 76,
+    cidade: "rio de janeiro"
+  },
+  {
+    nome: "Pessoa 8",
+    idade: 21,
+    cidade: "são paulo"
+  },
+  {
+    nome: "carlos",
+    idade: 43,
+    cidade: "recife"
+  },
+  {
+    nome: "carla",
+    idade: 65,
+    cidade: "perim mirim"
+  },
+   {
+    nome: "marcos pedro",
+    idade: 27,
+    cidade: "alcântara"
+   },
+   {
+     nome: "leticia",
+     idade:49,
+     cidade:"cafeteira"
+   },
+ 
+];
